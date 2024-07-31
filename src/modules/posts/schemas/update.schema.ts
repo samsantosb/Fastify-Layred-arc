3ff4dcece0ea04957fe82f323ea9ba66a49b3bcd
Yuri <yurisamp123@gmail.com>
2024-07-30 22:39:43 -0300

    fix update schema

--- src/modules/posts/schemas/update.schema.ts
+++ src/modules/posts/schemas/update.schema.ts
@@ -1,5 +1,5 @@
 import {z} from 'zod';
-import {createSchema} from './create.schema';
+
 import mongoose from 'mongoose';
 
 const isValidObjectId = (id: string) => mongoose.Types.ObjectId.isValid(id);
@@ -11,6 +11,12 @@ export const updateSchema = z
         message: 'Invalid ObjectId',
       }),
     }),
-    body: createSchema,
+    body: z.object({
+      title: z.string().min(1, 'Title is required'),
+      description: z.string().min(1, 'Description is required'),
+      category: z.string().min(1, 'Category is required'),
+      thumbnailUrl: z.string().min(1, 'Thumbnail URL is required').url('Invalid URL format'),
+      contentUrl: z.string().min(1, 'Content URL is required').url('Invalid URL format'),
+    }),
   })
   .transform(data => ({...data.body, ...data.params}));
