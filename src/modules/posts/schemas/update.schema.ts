583ab8a0f27b0eec26b889a5e664679d9c3fe67d
Yuri <yurisamp123@gmail.com>
2024-07-30 22:43:09 -0300

    find a better way to share post object

--- src/modules/posts/schemas/update.schema.ts
+++ src/modules/posts/schemas/update.schema.ts
@@ -1,6 +1,7 @@
 import {z} from 'zod';
 
 import mongoose from 'mongoose';
+import {postZodObject} from './create.schema';
 
 const isValidObjectId = (id: string) => mongoose.Types.ObjectId.isValid(id);
 
@@ -11,12 +12,6 @@ export const updateSchema = z
         message: 'Invalid ObjectId',
       }),
     }),
-    body: z.object({
-      title: z.string().min(1, 'Title is required'),
-      description: z.string().min(1, 'Description is required'),
-      category: z.string().min(1, 'Category is required'),
-      thumbnailUrl: z.string().min(1, 'Thumbnail URL is required').url('Invalid URL format'),
-      contentUrl: z.string().min(1, 'Content URL is required').url('Invalid URL format'),
-    }),
+    body: postZodObject,
   })
   .transform(data => ({...data.body, ...data.params}));
